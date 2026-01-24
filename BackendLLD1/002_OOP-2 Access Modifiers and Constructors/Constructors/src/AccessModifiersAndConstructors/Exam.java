ea33b8679e9f3efb3bb0b935ebc8c0069acc225f
Arjun Viswanath <arjunviswaa@gmail.com>
2026-01-24 14:03:17 +0530

    OOP-2 remaining changes

---
+++ BackendLLD1/002_OOP-2 Access Modifiers and Constructors/Constructors/src/AccessModifiersAndConstructors/Exam.java
@@ -0,0 +1,16 @@
+package AccessModifiersAndConstructors;
+
+public class Exam {
+    int exam_id;
+    int exam_score;
+
+    public Exam(int exam_id, int exam_score) {
+        this.exam_id = exam_id;
+        this.exam_score = exam_score;
+    }
+
+    public Exam(Exam other){
+        this.exam_id = other.exam_id;
+        this.exam_score = other.exam_score;
+    }
+}
--- BackendLLD1/002_OOP-2 Access Modifiers and Constructors/Constructors/src/AccessModifiersAndConstructors/Student.java
+++ BackendLLD1/002_OOP-2 Access Modifiers and Constructors/Constructors/src/AccessModifiersAndConstructors/Student.java
@@ -5,29 +5,37 @@ public class Student {
     int age;
     protected double psp;
     public String batch;
+    public Exam exam;
+    public static int noOfStudents;
 
     public Student() {
         this.name = "Sagar";
         this.age = 27;
         this.psp = 100.0;
+        noOfStudents++;
     }
 
     public Student(String name) {
         this.name = name;
         this.age = 27;
         this.psp = 100.0;
+        noOfStudents++;
     }
 
     public Student(String name, int age, double psp) {
         this.name = name;
         this.age = age;
         this.psp = psp;
+        noOfStudents++;
     }
 
     public Student(Student src) {
         this.name = src.name;
         this.age = src.age;
         this.psp = src.psp;
+        this.batch = src.batch;
+        this.exam = new Exam(src.exam);
+        noOfStudents++;
     }
 
     void Test() {
