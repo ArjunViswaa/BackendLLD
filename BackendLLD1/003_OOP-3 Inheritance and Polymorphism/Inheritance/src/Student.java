8b1e403ea491ff2f07543bb63c3a0318716869b9
Arjun Viswanath <arjunviswaa@gmail.com>
2026-01-26 10:53:58 +0530

    New changes to day 3

---
+++ BackendLLD1/003_OOP-3 Inheritance and Polymorphism/Inheritance/src/Student.java
@@ -0,0 +1,9 @@
+public class Student extends User {
+    private String batch;
+    double psp;
+
+    void attendClass() {
+        this.name = "Arjun";
+        System.out.println("Attended class");
+    }
+}
---
+++ BackendLLD1/003_OOP-3 Inheritance and Polymorphism/Inheritance/src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    private int id;
+    String name;
+    String password;
+
+    void login() {
+        System.out.println("Logged in!");
+    }
+}
